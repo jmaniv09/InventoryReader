6dc4bc0740ab142f955e2b9e5cb815e0a9df99d4
jmaniv09 <javier.maniviesa@decathlon.com>
2017-10-04 18:24:22 +0200

    Qr Libreria arreglada

--- app/src/main/java/app/num/barcodescannerproject/MainActivity.java
+++ app/src/main/java/app/num/barcodescannerproject/MainActivity.java
@@ -5,18 +5,24 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import com.google.zxing.Result;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
 
     private static final int ZBAR_SCANNER_REQUEST = 0;
     private static final int ZBAR_QR_SCANNER_REQUEST = 1;
     private Button btn;
     private Button list;
+    private boolean cameraActivated;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,29 +36,13 @@ public class MainActivity extends Activity {
         list.setTypeface(face);
     }
 
-    public void registerActivity(View v) {
-    	Intent intent = new Intent(MainActivity.this,DeviceActivity.class);
-        startActivity(intent);
-
-    }
-    
-    public void launchScanner(View v) {
-        if (isCameraAvailable()) {
-            Intent intent = new Intent(this, ZBarScannerActivity.class);
-            startActivityForResult(intent, ZBAR_SCANNER_REQUEST);
-        } else {
-            Toast.makeText(this, "Rear Facing Camera Unavailable", Toast.LENGTH_SHORT).show();
-        }
-    }
-
     public void launchQRScanner(View v) {
-        if (isCameraAvailable()) {
-            Intent intent = new Intent(this, ZBarScannerActivity.class);
-            intent.putExtra(ZBarConstants.SCAN_MODES, new int[]{Symbol.QRCODE});
-            startActivityForResult(intent, ZBAR_SCANNER_REQUEST);
-        } else {
-            Toast.makeText(this, "Rear Facing Camera Unavailable", Toast.LENGTH_SHORT).show();
-        }
+        mScannerView = new ZXingScannerView(this);
+        mScannerView.setAutoFocus(true);// Programmatically initialize the scanner view
+        setContentView(mScannerView);
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();
+        cameraActivated = true;
     }
     
     public void getAllDevices(View v) {
@@ -60,29 +50,50 @@ public class MainActivity extends Activity {
         startActivity(intent);
     }
 
-    public boolean isCameraAvailable() {
-        PackageManager pm = getPackageManager();
-        return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+
+        Log.e("handler", rawResult.getText()); // Prints scan results
+        Log.e("handler", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode
+        /*
+		 * switch (requestCode) { case ZBAR_SCANNER_REQUEST: case
+		 * ZBAR_QR_SCANNER_REQUEST:
+		 */
+        String str = rawResult.getText();
+        str = str.trim();
+        // str= str.trim(); // lo reinicia a un valor sin caracteres
+        // prohibidos.
+        Intent myIntent = new Intent(MainActivity.this, DeviceActivity.class);
+        myIntent.putExtra("device", str); //Optional parameters
+        MainActivity.this.startActivity(myIntent);
+        Log.w("qrinv1", "ha leido: " + str + " valor typeQR: " );
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mScannerView.removeAllViews(); //<- here remove all the views, it will make an Activity having no View
+                mScannerView.stopCamera(); //<- then stop the camera
+                resetView();
+            }
+        }, 100);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case ZBAR_SCANNER_REQUEST:
-            case ZBAR_QR_SCANNER_REQUEST:
-                if (resultCode == RESULT_OK) {
-                    //Toast.makeText(this, "Scan Result = " + data.getStringExtra(ZBarConstants.SCAN_RESULT), Toast.LENGTH_SHORT).show();
-                	String str = data.getStringExtra(ZBarConstants.SCAN_RESULT);
-                	Intent myIntent = new Intent(MainActivity.this, DeviceActivity.class);
-                	myIntent.putExtra("device", str); //Optional parameters
-                	MainActivity.this.startActivity(myIntent);
-                } else if(resultCode == RESULT_CANCELED && data != null) {
-                    String error = data.getStringExtra(ZBarConstants.ERROR_INFO);
-                    if(!TextUtils.isEmpty(error)) {
-                        Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
-                    }
-                }
-                break;
+    public void onBackPressed(){
+        if(!cameraActivated){
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }else{
+            resetView();
         }
+
+    }
+
+    public void resetView(){
+        cameraActivated = false;
+        mScannerView.stopCamera();
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
     }
 }
